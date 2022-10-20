aec1c1f7abf25642b1580f8cb40bde3268028307
Andrew Peirs <peirs.andrew@gmail.com>
2022-10-20 11:10:58 +0100

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+/** @jsxImportSource @emotion/react */
+
+function App() {
+  return (
+    <div css={{ display: "flex", justifyContent: "center" }}>
+      <h1>BASIC NFT 4000</h1>
+    </div>
+  );
+}
+
+export default App;
